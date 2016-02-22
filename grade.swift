cbad2b0db2150843f3c1e3d8959cbf66dde32749
cph52b <cph52b@mail.missouri.edu>
2016-02-21 20:24:03 -0600

    Update grade.swift

--- grade.swift
+++ grade.swift
@@ -12,14 +12,21 @@ var dictionary: [String] =
 
 var count = dictionary.count
 
-var string = "They bought alabama hot pockets"
+var string = "anal anal anal anilingus"
 
-var value = 0;
-var autoGrade = 0;
+var value = 0
+var autoGrade = 0
+var humanGrade = 0
+var weightedTotal : Double
+var weightedAutoGrade : Double
+var weightedHumGrade : Double
 
 for var index = 0; index < count; index++ {
     if string.rangeOfString(dictionary[index]) != nil {
+            print("\(dictionary[index])")
             value++
+            print("\(dictionary[index]) is a flagged word")
+            print(value)
     }
 }
 
@@ -39,4 +46,29 @@ else if value == 0 {
    autoGrade = 100
 }
 
-print(autoGrade)
+//if (goodPost)
+    humanGrade = 100
+//else
+    //humanGrade = 0
+
+weightedAutoGrade = 0.25 * Double(autoGrade)
+weightedHumGrade = 0.75 * Double(humanGrade)
+weightedTotal = weightedAutoGrade + weightedHumGrade
+
+print("Your post has a grade of \(weightedTotal)")
+
+if weightedTotal >= 90 {
+    print("Make post")
+    //edit?
+}
+else if weightedTotal < 90 && weightedTotal >= 70 {
+    print("yellow “Make post” button")
+    //edit?
+}
+else if weightedTotal < 70 && weightedTotal >= 50 {
+    print("red make post button")
+    //edit?
+}
+else if weightedTotal < 50 {
+    print("Please edit your post before continuing")
+}
